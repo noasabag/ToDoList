da6f5b29f3785c4d5113d26ae4958f322935a85a
unknown <noa1605@gmail.com>
2022-03-23 21:05:33 +0200

    change pull req

--- src/Todo.js
+++ src/Todo.js
@@ -1,47 +1,51 @@
-import './App.css';
 import React from 'react';
 import List from './List.js'
+import './App.css';
 import './ToDo.css'
 
 class Todo extends React.Component{
   constructor(props){
     super(props)
-    this.state={list:[]}
-    let a = ''
+    this.state = { list: [] }
+    let missionDescription = ''
   }
   
   addMission = (event) => {
     event.preventDefault();
-    let b=Math.random()
+    let missionId= Math.random()
     this.setState( {
-      list: [...this.state.list,{id:b, description:this.state.a, completed:false}]
-    }) 
-     } 
+      list: [ ...this.state.list,
+         { id: missionId, description: this.state.missionDescription, completed: false } ]
+                  }) 
+  } 
 
 
      InputValue=(event)=>{
-      this.setState({a:event.target.value})
+      this.setState({ missionDescription: event.target.value })
      }
 
 
      removeMission=(missionID)=>{
-      let newARR = this.state.list.filter(object=>{
-      if(object.id===missionID) return false
-      else return true
-    })
-    this.setState({list:newARR})
-     }
+      const newARR = this.state.list.filter(object=>
+      object.id!==missionID
+        )
+       this.setState({ list: newARR })
+      }
 
   changeState=(id)=>{       //mission completes or not
-   var a =this.state.list.length
-    for (let i = 0; i < a; i++) {
-      if( this.state.list[i].id===id){
-        this.setState({list:
-        [  ...this.state.list.slice(0,i),
-          Object.assign({}, this.state.list[i], {completed:!this.state.list[i].completed}), ...this.state.list.slice(i+1)]
+    this.state.list.length.map((object, index )=> {
+      if( object.id===id){
+        this.setState({ list: 
+        [  
+          ...this.state.list.slice(0,index),
+          Object.assign({},
+            object, 
+          { completed: !object.completed }),
+          ...this.state.list.slice(index+1)
+        ]
         })
        }
-    }
+    })
   }
   
   render(){
@@ -56,7 +60,7 @@ return(
             <ul>
                {
                 this.state.list.map(element => {
-             return (<List key = {element.id} changeState={this.changeState} listItem={element} removeMission={this.removeMission} />
+             return (<List key = {element.id} changeState = { this.changeState } listItem = { element } removeMission={this.removeMission} />
 )
             }
                   ) }
